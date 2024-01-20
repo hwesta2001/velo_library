e848d753e6221911bd2a7e1de48664e52eeaa957
hwesta2001 <hasanhw@gmail.com>
2024-01-21 02:55:29 +0300

    first night.

---
+++ 1-VeloLibrary/scripts/LibraryManager.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace VeloLibrary
+{
+    internal class LibraryManager
+    {
+        public string jsonFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "books.json");
+        public List<Book> books = new List<Book>();
+
+        public void CreateLibrary()
+        {
+            if (File.Exists(jsonFilePath))
+            {
+                books.Clear();
+                books = ReadJsonToList(jsonFilePath);
+                Console.WriteLine("Existing Library Loaded.");
+            }
+            else
+            {
+                InitJsonFile();
+                Console.WriteLine("New Library Created.");
+            }
+
+            ShowBookList();
+        }
+
+        public void ShowBookList()
+        {
+            Console.WriteLine("Book List:");
+            foreach (var book in books)
+            {
+                Console.WriteLine($"BookId: {book.BookId}, Title: {book.Title}, Author: {book.Author}, ISBN: {book.ISBN}, StockAmount: {book.StockAmount}, LentAmount: {book.LentAmount}");
+            }
+        }
+
+        public void CreateAndAddBookToLibrary()
+        {
+            Book book = new Book();
+            Console.WriteLine("Book Title: ");
+            book.Title = Console.ReadLine();
+            Console.WriteLine("Book Author: ");
+            book.Author = Console.ReadLine();
+            Console.WriteLine("Book ISBN: ");
+            book.ISBN = Console.ReadLine();
+            Console.WriteLine("Book added");
+            AddBookToLibrary(book);
+        }
+
+        public bool AddExistingBook(int id)
+        {
+            if (id < 0 || id >= books.Count)
+            {
+                return false;
+            }
+            else
+            {
+                AddBookToLibrary(GetBookWithId(id));
+                return true;
+            }
+        }
+
+        public Book GetBookWithId(int id)
+        {
+            return books[id];
+        }
+
+        private void InitJsonFile()
+        {
+            Book dummyBook0 = new Book
+            {
+                BookId = 10,
+                Title = "Dummy_Book",
+                Author = "Dummy_Author",
+                ISBN = "000-0000000000",
+                StockAmount = 0,
+                LentAmount = 0
+            };
+
+            Book dummyBook01 = new Book
+            {
+                BookId = 11,
+                Title = "Dummy_Book1",
+                Author = "Dummy_Author1",
+                ISBN = "000-0000000001",
+                StockAmount = 0,
+                LentAmount = 0
+            };
+
+            books.Clear();
+            // Add 10 of dummy_book
+            for (int i = 0; i < 10; i++)
+            {
+                AddBookToLibrary(dummyBook0);
+            }
+            AddBookToLibrary(dummyBook01);
+        }
+
+        public void AddBookToLibrary(Book book)
+        {
+            if (books.Contains(book))
+            {
+                book.StockAmount++;
+            }
+            else
+            {
+                book.StockAmount = 1;
+                books.Add(book);
+            }
+            RefreshBooksJson();
+        }
+
+        public void RemoveBookToLibrary(Book book)
+        {
+            if (books.Contains(book))
+            {
+                book.StockAmount--;
+                if (book.StockAmount <= 0)
+                {
+                    books.Remove(book);
+                    Console.WriteLine(book.Title + " removed from library.");
+                }
+            }
+            RefreshBooksJson();
+        }
+
+        public void RefreshBooksJson()
+        {
+            foreach (Book book in books)
+            {
+                //bookid leri indexlere eşleyerek id ile kitap çağrmayı kolaylaştırabiliriz.
+                book.BookId = books.IndexOf(book);
+            }
+            File.WriteAllText(jsonFilePath, JsonConvert.SerializeObject(books, Formatting.Indented));
+        }
+
+        List<Book> ReadJsonToList(string filePath)
+        {
+            return JsonConvert.DeserializeObject<List<Book>>(File.ReadAllText(filePath));
+        }
+
+
+    }
+}
---
+++ 1-VeloLibrary/scripts/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VeloLibrary
+{
+    internal class Program
+    {
+        static LibraryManager libraryManager;
+        static void Main(string[] args)
+        {
+            libraryManager = new LibraryManager();
+            libraryManager.CreateLibrary();
+            Console.WriteLine("\nWelcome to Velo Library");
+
+            Operations();
+        }
+
+        private static void Operations()
+        {
+            Console.WriteLine("\nChoose an option from the following list:");
+            Console.WriteLine("\ts - Show book list.");
+            Console.WriteLine("\ta - Add a book to library");
+            Console.WriteLine("\trm - Remove book from library");
+            Console.WriteLine("\tf - Find a book in library");
+            Console.WriteLine("\tl - Lend a book from library");
+            Console.WriteLine("\trt - Return a book to library");
+            Console.WriteLine("\tc - Clear \tx - Exit");
+            Console.WriteLine("What do you want to do? ");
+            switch (Console.ReadLine())
+            {
+                case "x":
+                    Environment.Exit(0);
+                    break;
+                case "c":
+                    Console.Clear();
+                    Operations();
+                    break;
+                case "s":
+                    libraryManager.ShowBookList();
+                    Operations();
+                    break;
+                case "a":
+                    libraryManager.ShowBookList();
+                    Console.WriteLine("Add an existing book? (y)es or (n)no?");
+                    switch (Console.ReadLine())
+                    {
+                        case "y":
+                        case "yes":
+                            TryAddExistingBook();
+                            break;
+                        case "n":
+                        case "no":
+                            libraryManager.CreateAndAddBookToLibrary();
+                            break;
+                        default:
+                            Console.WriteLine("Invalid entry");
+                            break;
+                    }
+                    Operations();
+                    break;
+                default:
+                    Console.WriteLine("Would you like to continue the operation? (y)es");
+                    if (Console.ReadLine() == "y" || Console.ReadLine() == "yes")
+                    {
+                        Operations();
+                    }
+                    else
+                    {
+                        Environment.Exit(0);
+                    }
+                    break;
+            }
+
+        }
+
+        private static void TryAddExistingBook()
+        {
+            Console.WriteLine("Write BookId you want to add ");
+            int id = Convert.ToInt32(Console.ReadLine());
+            if (libraryManager.AddExistingBook(id))
+            {
+                Console.WriteLine("\nOne more '" + libraryManager.GetBookWithId(id).Title + "' added.");
+                Console.WriteLine("Press a key to continue.");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Invalid book id. Do you want to try again? (y)es?");
+                if (Console.ReadLine() == "y" || Console.ReadLine() == "yes")
+                {
+                    libraryManager.ShowBookList();
+                    TryAddExistingBook();
+                }
+                else
+                {
+                    Operations();
+                }
+            }
+        }
+    }
+}
